9fd590b1984e04d31a8a456a3553c54b859d2e59
AndriyBrine <brineandriy@gmail.com>
2026-05-15 09:01:13 -0300

    Segundo commit (slide 25/135) https://drive.google.com/file/d/16XnB2gVx83is-_qI5FDg3CvWrOkfKHXd/view?usp=drive_link

--- app/src/main/java/com/andbr/exercicio4/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/andbr/exercicio4/ui/auth/RegisterFragment.kt
@@ -6,13 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.andbr.exercicio4.R
-import com.andbr.exercicio4.databinding.FragmentLoginBinding
 import com.andbr.exercicio4.databinding.FragmentRegisterBinding
 
+class RegisterFragment : Fragment() {
 
-class LoginFragment : Fragment() {
-
-    private var _binding: FragmentLoginBinding? = null
+    private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -21,7 +19,7 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
 
