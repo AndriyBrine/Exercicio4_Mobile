74f28f5d9df105c1e48d972124bab423198b1262
AndriyBrine <brineandriy@gmail.com>
2026-05-21 13:07:05 -0300

    Terceiro commit (slide 40/135) https://drive.google.com/file/d/16XnB2gVx83is-_qI5FDg3CvWrOkfKHXd/view?usp=drive_link Fiz at eo passo 65

--- app/src/main/java/com/andbr/exercicio4/ui/SplashFragment.kt
+++ app/src/main/java/com/andbr/exercicio4/ui/SplashFragment.kt
@@ -1,12 +1,17 @@
 package com.andbr.exercicio4.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.andbr.exercicio4.R
 import com.andbr.exercicio4.databinding.FragmentSplashBinding
+
+
 
 class SplashFragment : Fragment() {
 
@@ -18,10 +23,18 @@ class SplashFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
-        _binding = FragmentSplashBinding.inflate(R.layout.fragment_splash, container, false)
+        _binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({checkAuth()}, 3000)
+    }
+
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_autentication)
     }
 
     override fun onDestroyView() {
