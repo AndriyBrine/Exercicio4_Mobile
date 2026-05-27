67b69434a893c05873bf9b80ec072b9627608a66
AndriyPolastrelli <brineandriy@gmail.com>
2026-05-26 23:02:29 -0300

    commit (slide 40/135) https://drive.google.com/file/d/16XnB2gVx83is-_qI5FDg3CvWrOkfKHXd/view?usp=drive_link Fiz at eo passo 65

---
+++ app/src/main/java/com/andbr/exercicio4/ui/DoneFragment.kt
@@ -0,0 +1,32 @@
+package com.andbr.exercicio4.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andbr.exercicio4.R
+import com.andbr.exercicio4.databinding.FragmentDoneBinding
+import com.andbr.exercicio4.databinding.FragmentTodoBinding
+
+
+class DoneFragment : Fragment() {
+
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentDoneBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
